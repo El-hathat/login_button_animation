50c3961cb400d8d7acaedc7a5e11735921e38ad0
El-hathat <mohamedelhathat9@gmail.com>
2023-08-04 14:33:18 +0200

    first

---
+++ ButtonAnnim/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Bauhaus 93", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(117, 158)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(106, 27)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Connexion"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(91, 48)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(162, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(91, 90)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(162, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(349, 270)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+
+End Class
---
+++ ButtonAnnim/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+    Dim email As String = "aaa"
+    Dim mdp As String = "hathat"
+    Dim b As Boolean = False
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+        If email = TextBox1.Text And mdp = TextBox2.Text Then
+            b = True
+            Button1.Location = New Point(117, 158)
+            'Button1.Size = New Size(106, 27)
+            ' Button1.Visible = True
+        Else
+            'Button1.Size = New Size(27, 27)
+            'Button1.Visible = False
+            b = False
+        End If
+
+    End Sub
+
+    
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        If email = TextBox1.Text And mdp = TextBox2.Text Then
+            b = True
+            Button1.Location = New Point(117, 158)
+            'Button1.Size = New Size(106, 27)
+            ' Button1.Visible = True
+        Else
+            'Button1.Visible = False
+            'Button1.Size = New Size(27, 27)
+            b = False
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        MsgBox("La Connexion est terminer avec succees")
+        Me.BackColor = Color.Chocolate
+        ' Me.Close()
+    End Sub
+
+    Private Sub Button1_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Button1.MouseMove
+        If b = False Then
+            If Button1.Location = New Point(234, 158) Then
+
+                Button1.Location = New Point(10, 158)
+            ElseIf Button1.Location = New Point(117, 158) Then
+
+                Button1.Location = New Point(234, 158)
+            ElseIf Button1.Location = New Point(10, 158) Then
+
+                Button1.Location = New Point(117, 158)
+            End If
+
+            'Button1.Size = New Size(27, 27)
+
+
+        End If
+    End Sub
+End Class
